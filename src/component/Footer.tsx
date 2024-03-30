fb214afdcbeb8f025a63047c8f4d846d665bd1de
Mahesh <mj7267889@gmail.com>
2024-03-30 16:16:39 +0530

    changes

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Col, Row } from 'react-bootstrap'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='bg-secondary text-white p-3 '>
+      <Row>
+        <Col className='d-flex justify-content-center' md={4}>
+         Logo
+        </Col>
+        <Col className='d-flex justify-content-center' md={4}>
+          hh
+        </Col>
+        <Col className='d-flex justify-content-center' md={4}>
+          hh
+        </Col>
+      </Row>
+    </div>
   )
 }
 
