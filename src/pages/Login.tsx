17176093c164d4e68ff765f04b1088dfe1f64868
Mahesh <mj7267889@gmail.com>
2024-03-30 16:43:10 +0530

    login form changeh

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,8 +3,8 @@ import { Button, Card, CardBody, CardHeader, Col, Form, InputGroup, Row } from '
 
 const Login = () => {
   return (
-    <div className='p-4'>
-      <div className='py-4 d-flex justify-content-end'>
+    <div className='p-5'>
+      <div className='pb-3 d-flex justify-content-end'>
         <a href="/">
           <Button className='rounded-pill btn btn-dark px-4'>
             Back
@@ -13,57 +13,55 @@ const Login = () => {
       </div>
       <Card className='shadow py-5'>
         <CardBody>
+          <p className='text-center fs-3'>Login Form</p>
           <div className='d-flex justify-content-center'>
-            <div>
-              <Card className='shadow-lg'>
-                <CardHeader className='text-center'>
-                  <p className='fs-3 my-0'>Login Form</p>
-                </CardHeader>
-                <CardBody className='p-4'>
-                  <Row className='p-2'>
-                    <Col md={12}>
-                      <Form.Label className="d-flex" htmlFor="email">
-                        {"Email"}<span className="text-danger">*</span>
-                      </Form.Label>
-                      <InputGroup className="d-flex">
-                        <Form.Control
-                          name="email"
-                          type="email"
-                          id="email"
-                          placeholder={"Enter email address"}
-                        />
-                      </InputGroup>
-                    </Col>
-                  </Row>
-                  <Row className='p-2'>
-                    <Col md={12}>
-                      <Form.Label className="d-flex" htmlFor="password">
-                        {"Password"}<span className="text-danger">*</span>
-                      </Form.Label>
-                      <InputGroup className="d-flex">
-                        <Form.Control
-                          name="password"
-                          type="password"
-                          id="password"
-                          placeholder={"Enter password"}
-                        />
-                      </InputGroup>
-                    </Col>
-                  </Row>
-                  <Row className='p-2'>
-                    <Col md={12} className='d-flex justify-content-center'>
-                      <Button type='submit' className='rounded-pill btn-primary px-4 w-100'>
-                        Login
-                      </Button>
-                    </Col>
-                  </Row>
-                  <Row className='p-2'>
-                    <Col md={12}>
-                     <a href="#">Forgot your password</a>
-                    </Col>
-                  </Row>
-                </CardBody>
-              </Card>
+            <div className='py-3'>
+            {/* <Card className='shadow-lg'>
+              <CardBody className='p-4'> */}
+                <Row className='p-2'>
+                  <Col md={12}>
+                    <Form.Label className="d-flex" htmlFor="email">
+                      {"Email"}<span className="text-danger">*</span>
+                    </Form.Label>
+                    <InputGroup className="d-flex">
+                      <Form.Control
+                        name="email"
+                        type="email"
+                        id="email"
+                        placeholder={"Enter email address"}
+                      />
+                    </InputGroup>
+                  </Col>
+                </Row>
+                <Row className='p-2'>
+                  <Col md={12}>
+                    <Form.Label className="d-flex" htmlFor="password">
+                      {"Password"}<span className="text-danger">*</span>
+                    </Form.Label>
+                    <InputGroup className="d-flex">
+                      <Form.Control
+                        name="password"
+                        type="password"
+                        id="password"
+                        placeholder={"Enter password"}
+                      />
+                    </InputGroup>
+                  </Col>
+                </Row>
+                <Row className='p-2'>
+                  <Col md={12} className='d-flex justify-content-center'>
+                    <Button type='submit' className='rounded-pill btn-primary px-4 w-100'>
+                      Login
+                    </Button>
+                  </Col>
+                </Row>
+                <Row className='px-2 py-3'>
+                  <Col md={12}>
+                    <a href="#">Forgot your password</a>
+                  </Col>
+                </Row>
+              {/* </CardBody>
+            </Card> */}
             </div>
           </div>
         </CardBody>
